7b723ec2bf123c2e4c3739c18572b519c487df26
ThomasFarr <114097013+ThomasFarr@users.noreply.github.com>
2023-01-19 12:40:21 -0500

    my information v2

---
+++ myInfov2.py
@@ -0,0 +1,13 @@
+# CS175L-01
+
+#Your first name and Last name
+
+#myInfo
+
+print ("Thomas Farrell")
+
+print ("Garryford Drive, Middletown, 07748")
+
+print ("732-788-7763")
+
+print ("Computer Science")
