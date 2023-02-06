2229931307e7b66bc4b2373043eb33340c2d1c1a
ThomasFarr <114097013+ThomasFarr@users.noreply.github.com>
2023-02-06 12:47:11 -0500

    restuarant version 2

--- restaurant.py
+++ restaurant.py
@@ -6,36 +6,41 @@ vegetarian = False
 vegan = False
 gluten_free = False
 
-vegetarian = input('Is anyone in your party a vegetarian? ')
-if vegetarian == 'yes':
-    vegetarian = True
-else:
-    vegetarian = False
+keep_going = 'yes'
+
+while keep_going == 'yes':
+    vegetarian = input('Is anyone in your party a vegetarian? ')
+    if vegetarian == 'yes':
+        vegetarian = True
+    else:
+        vegetarian = False
 
 
 
-vegan = input('Is anyone in your party a vegan? ')
-if vegan == 'yes':
-    vegan = True
-else:
-    vegan = False
+    vegan = input('Is anyone in your party a vegan? ')  
+    if vegan == 'yes':
+        vegan = True
+    else:
+        vegan = False
 
 
-gluten_free = input('Is anyone in your party gluten-free? ')
-if gluten_free == 'yes':
-    gluten_free = True
-else:
-    gluten_free = False
+    gluten_free = input('Is anyone in your party gluten-free? ')
+    if gluten_free == 'yes':
+        gluten_free = True
+    else:
+        gluten_free = False
     
-print("Here are your restuarant choices.")
+    print("Here are your restuarant choices.")
 
-if vegetarian == False and vegan == False and gluten_free == False:
-    print(" Joe's Gourmet Burgers")
-elif vegan == False and gluten_free == False:
-    print(" Mama's Fine Italian")
-elif vegan == False:
-    print(" Main Street Pizza Company")
+    if vegetarian == False and vegan == False and gluten_free == False:
+        print(" Joe's Gourmet Burgers")
+    elif vegan == False and gluten_free == False:
+        print(" Mama's Fine Italian")
+    elif vegan == False:
+        print(" Main Street Pizza Company")
 
 
-print(" Corner Cafe")
-print(" The Chef's Kitchen")
+    print(" Corner Cafe")
+    print(" The Chef's Kitchen")
+
+    keep_going = input("Enter 'yes' if you would like to do another search (enter 'no' to end): ")
